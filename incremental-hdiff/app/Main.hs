af8878f869e5cbba5d180ebd2a28061f0cae77f1
Jort van Gorkum <jortvangorkum@gmail.com>
2021-11-16 12:56:25 +0100

    Setup command line parser

--- incremental-hdiff/app/Main.hs
+++ incremental-hdiff/app/Main.hs
@@ -1,6 +1,41 @@
 module Main where
 
 import           Lib
+import           Options.Applicative
+
+data Options
+  = AST  { optFileA :: FilePath
+         }
+  | Diff { optFileA :: FilePath
+         , optFileB :: FilePath
+         }
+
+astOpts :: Parser Options
+astOpts = AST <$> strArgument (metavar "FILE")
+
+diffOpts :: Parser Options
+diffOpts =
+  Diff <$> strArgument (metavar "OLDFILE")
+       <*> strArgument (metavar "NEWFILE")
+
+parseOptions :: Parser Options
+parseOptions = subparser
+  (  command "ast" (info astOpts ( progDesc "Parses and displays an AST"))
+  <> command "diff" (info diffOpts (progDesc "Runs Diff on targets"))
+  ) <|> diffOpts
+
+cmdOptions :: ParserInfo Options
+cmdOptions = info (parseOptions <**> helper)
+  (  fullDesc
+  <> progDesc  "Runs hdiff with specified command, 'diff' is the default command"
+  )
+
+showFile :: Options -> IO ()
+showFile (AST file)         = putStrLn $ "AST: " ++ file
+showFile (Diff fileA fileB) = putStrLn $ "Diff: " ++ fileA ++ ", " ++ fileB
 
 main :: IO ()
-main = someFunc
+main = do
+  options <- execParser cmdOptions
+  showFile options
+  return ()
