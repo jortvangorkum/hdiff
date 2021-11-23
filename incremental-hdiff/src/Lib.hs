34a0265f3ea392f6814dcc4cfec966d3c2b32790
Jort van Gorkum <jortvangorkum@gmail.com>
2021-11-23 12:35:07 +0100

    Added a fold for hashes

--- incremental-hdiff/src/Lib.hs
+++ incremental-hdiff/src/Lib.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ConstraintKinds     #-}
+{-# LANGUAGE RankNTypes          #-}
 module Lib where
 
 import           CommandLine                (Options (..))
@@ -8,11 +9,11 @@ import           Data.Functor.Const         (Const (..))
 import qualified Data.Map                   as M
 import           Data.Maybe                 (fromJust)
 import           Data.Word                  (Word64)
-import           Generics.Simplistic        (repLeavesList)
+import           Generics.Simplistic        (SRep, repLeavesList)
 import           Generics.Simplistic.Deep
 import           Generics.Simplistic.Digest (Digest (getDigest))
 import qualified Generics.Simplistic.Digest as D
-import           Generics.Simplistic.Util   (exElim)
+import           Generics.Simplistic.Util   (Exists (Exists), exElim)
 import           Languages.Interface
 import           Languages.Main
 import           Options.Applicative        (execParser)
@@ -25,19 +26,44 @@ mainAST ext opts = withParsed1 ext mainParsers (optFileA opts)
     print fa
     return ExitSuccess
 
-foldToMap :: M.Map String ix -> PrepFix a kappa fam ix -> M.Map String ix
-foldToMap m (Hole' _ _) = m
-foldToMap m (Prim' ann x) = M.insert k x m
+holesFold :: (Monoid m, Foldable f)
+          => (forall x a . ann a -> x -> m)
+          -> (forall a   . ann a -> m -> m)
+          -> HolesAnn kappa fam ann f a
+          -> m
+holesFold f _ (Hole' ann x) = foldMap (f ann) x
+holesFold f _ (Prim' ann x) = f ann x
+holesFold f g (Roll' ann x) = g ann ms
   where
-    (Const (PrepData treeDigest _ treeVal)) = ann
-    k = show $ getDigest treeDigest
-foldToMap m (Roll' ann x) = ms
-  where
-    -- TODO: Trying to find out where the value of the node is stored and put that in the Map as value
-    ms = M.unions $ map (exElim (foldToMap m)) xs
-    (Const (PrepData treeDigest _ treeVal)) = ann
-    k = show $ getDigest treeDigest
     xs = repLeavesList x
+    ms = mconcat $ map (exElim (holesFold f g)) xs
+
+
+testFold :: PrepFix a kappa fam ix -> [String]
+testFold = holesFold f g
+  where
+    f ann x = [take 5 (getHash ann)]
+    g ann m = [take 5 (getHash ann)] <> m
+
+getHash :: Const (PrepData a) ix -> String
+getHash (Const (PrepData treeDigest _ _)) = show $ getDigest treeDigest
+
+-- foldToMap :: M.Map String (PrepFix a kappa fam ix)
+--           -> PrepFix a kappa fam ix
+--           -> M.Map String (PrepFix a kappa fam ix)
+-- foldToMap m h@(Hole' ann _) = M.insert k h m
+--   where
+--     k = getHash ann
+-- foldToMap m p@(Prim' ann x) = M.insert k p m
+--   where
+--     k = getHash ann
+-- foldToMap m r@(Roll' ann x) = M.insert k r m <> ms
+--   where
+--     -- TODO: Trying to find out where the value of the node is stored and put that in the Map as value
+--     ms = M.unions $ map f xs
+--     f = exElim (foldToMap m)
+--     k = getHash ann
+--     xs = repLeavesList x
 
 mainDiff :: Maybe String -> Options -> IO ExitCode
 mainDiff ext opts = withParsed2 ext mainParsers (optFileA opts) (optFileB opts)
@@ -45,8 +71,8 @@ mainDiff ext opts = withParsed2 ext mainParsers (optFileA opts) (optFileB opts)
     let decFa = decorate fa
     print decFa
 
-    let treeMap = foldToMap M.empty decFa
-    print treeMap
+    let hashes = testFold decFa
+    print hashes
 
     return ExitSuccess
 
