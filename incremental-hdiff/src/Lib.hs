1222915fb39e518c686e0de4ea20d6054e874195
Jort van Gorkum <jortvangorkum@gmail.com>
2021-11-25 15:27:07 +0100

    Written a fold for PrepFix

--- incremental-hdiff/src/Lib.hs
+++ incremental-hdiff/src/Lib.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ConstraintKinds     #-}
+{-# LANGUAGE GADTs               #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Lib where
 
 import           CommandLine                (Options (..))
 
+import           Control.Monad.Identity     (Identity (runIdentity))
 import           Data.Functor.Const         (Const (..))
+import           Data.Functor.Identity
 import qualified Data.Map                   as M
 import           Data.Maybe                 (fromJust)
 import           Data.Word                  (Word64)
-import           Generics.Simplistic        (Generic (Rep), SRep, V1, repLeaves,
-                                             repLeavesList, toS)
-import           Generics.Simplistic.Deep
+import           Generics.Simplistic        (Generic (Rep), SRep, V1 (..),
+                                             repLeaves, repLeavesList, repMap,
+                                             repMapM, toS)
+import           Generics.Simplistic.Deep   (CompoundCnstr,
+                                             HolesAnn (Hole', Prim', Roll'),
+                                             PrimCnstr, SFixAnn, cataM)
 import           Generics.Simplistic.Digest (Digest (getDigest))
 import qualified Generics.Simplistic.Digest as D
 import           Generics.Simplistic.Util   (All, Exists (Exists), exElim)
@@ -28,51 +34,64 @@ mainAST ext opts = withParsed1 ext mainParsers (optFileA opts)
     print fa
     return ExitSuccess
 
-holesFold :: (Monoid m, Foldable f)
-          => (forall x a . ann a -> x -> m)
-          -> (forall a   . ann a -> m -> m)
-          -> HolesAnn kappa fam ann f a
-          -> m
-holesFold f _ (Hole' ann x) = foldMap (f ann) x
-holesFold f _ (Prim' ann x) = f ann x
-holesFold f g (Roll' ann x) = g ann ms
-  where
-    xs = repLeavesList x
-    ms = mconcat $ map (exElim (holesFold f g)) xs
+data Tree a = Leaf
+            | Node (Tree a) a (Tree a)
 
+cataT :: b
+      -> (b -> a -> b -> b)
+      -> Tree a -> b
+cataT f _ Leaf         = f
+cataT f g (Node l x r) = g (cataT f g l) x (cataT f g r)
 
-testFold :: PrepFix a kappa fam ix -> [String]
-testFold = holesFold f g
+cataTM :: Monad m => m b
+       -> (m b -> a -> m b -> m b)
+       -> Tree a -> m b
+cataTM f _ Leaf         = f
+cataTM f g (Node l x r) = g (cataTM f g l) x (cataTM f g r)
+
+testTree :: Tree Int
+testTree = Node (Node Leaf 1 Leaf) 2 Leaf
+
+testFoldT :: Tree Int -> [Int]
+testFoldT = cataT leaf node
   where
-    f ann x = [take 5 (getHash ann)]
-    g ann m = [take 5 (getHash ann)] <> m
+    leaf = [0]
+    node l x r = l ++ [x] ++ r
+
+testFoldTM :: Tree Int -> [Int]
+testFoldTM = runIdentity . cataTM leaf node
+  where
+    leaf = Identity [0]
+    node :: Monad m => m [Int] -> Int -> m [Int] -> m [Int]
+    node l x r = do
+      l' <- l
+      r' <- r
+      return $ l' ++ [x] ++ r'
+
+cataP :: Monoid m =>
+      (forall x . Const (PrepData a) x -> V1 x -> m)
+      -> (forall x . PrimCnstr kappa fam x => Const (PrepData a) x -> x -> m)
+      -> (forall x . CompoundCnstr kappa fam x => Const (PrepData a) x -> m -> m)
+      -> PrepFix a kappa fam ix -> m
+cataP f g h (Hole' ann x) = f ann x
+cataP f g h (Prim' ann x) = g ann x
+cataP f g h (Roll' ann x) = h ann x'
+  where
+    x' = repLeaves (cataP f g h) (<>) mempty x
+
+testFold :: PrepFix a kappa fam ix -> M.Map String Int
+testFold = cataP f g h
+  where
+    f ann x = M.empty
+    g ann x = M.insert (take 5 (getHash ann)) 0 M.empty
+    h ann x = M.insert (take 5 (getHash ann)) xs x
+      where
+        xs = if not $ null (M.elems x)
+             then 1 + maximum (M.elems x)
+             else 0
 
 getHash :: Const (PrepData a) ix -> String
 getHash (Const (PrepData treeDigest _ _)) = show $ getDigest treeDigest
-
-{-
-  The problem is that because of SRep expecting a (* -> *) type.
-  PrepFix loses the type ix, which is then needed in the result of the Map.
-
-  So we need to find a way to get type ix out of SRep
--}
-repList :: forall a kappa fam ix . SRep (PrepFix a kappa fam) (Rep ix) -> [PrepFix a kappa fam ix]
-repList s = ss -- repLeaves ((:[]) . Exists) (++) []
-  where
-    f :: PrepFix a kappa fam x -> [PrepFix a kappa fam ix]
-    f x = [x]
-    g :: [PrepFix a kappa fam ix] -> [PrepFix a kappa fam ix] -> [PrepFix a kappa fam ix]
-    g = (++)
-    ss :: [PrepFix a kappa fam ix]
-    ss = repLeaves f g [] s
-
-foldToMap :: forall kappa fam ix . PrepFix () kappa fam ix -> M.Map String (PrepFix () kappa fam ix)
-foldToMap h@(Hole' ann x) = M.insert (getHash ann) h M.empty
-foldToMap p@(Prim' ann x) = M.insert (getHash ann) p M.empty
-foldToMap r@(Roll' ann x) = M.insert (getHash ann) r m
-  where
-    m = mconcat $ foldToMap <$> xs
-    xs = repList x
 
 mainDiff :: Maybe String -> Options -> IO ExitCode
 mainDiff ext opts = withParsed2 ext mainParsers (optFileA opts) (optFileB opts)
@@ -80,7 +99,7 @@ mainDiff ext opts = withParsed2 ext mainParsers (optFileA opts) (optFileB opts)
     let decFa = decorate fa
     print decFa
 
-    let hashMap = foldToMap decFa
+    let hashMap = testFold decFa
     print hashMap
 
     return ExitSuccess
